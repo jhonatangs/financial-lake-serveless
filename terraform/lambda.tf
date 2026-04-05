f1214f75fb94cc83d4ddbd842b87281caf504b13
jhonatangs <jhonatangs1@gmail.com>
2026-04-05 03:42:57 -0300

    fix(infra): fix yfinance lambda handler name

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "producer_yfinance" {
   function_name = "financial-producer-yfinance"
   role          = aws_iam_role.producer_role.arn
-  handler       = "producer.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
   timeout       = 60 
   memory_size   = 128
